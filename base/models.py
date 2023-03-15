9bfe5004507810dab4b6932ec4ef8f15f23a733e
sarikaksstrcs <sarika.ks.official@gmail.coms>
2023-03-15 14:41:55 +0530

    added event table

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Event(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null = True,blank=True)
+    event_name = models.CharField(max_length=200)
+    event_date = models.DateTimeField()
+    venue = models.CharField(max_length=200)
+    parking = models.BooleanField(default=False)
+    parking_capacity = models.PositiveIntegerField()
+    tickets_available = models.PositiveIntegerField()
+    tickets_sold = models.PositiveIntegerField()
+    complete = models.BooleanField(default= False)
+    booking_open = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.event_name
+    
+    
